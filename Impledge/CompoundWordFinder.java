4712472466f3bc42874242fc52a7101e0eaac4d8
Suhaib Mirza <suhaibmirza715@gmail.com>
2024-09-02 18:15:56 +0530

    updated code

--- Impledge/CompoundWordFinder.java
+++ Impledge/CompoundWordFinder.java
@@ -2,50 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class CompoundWordFinder {
-    public static void main(String[] args) throws IOException {
-        List<String> words = new ArrayList<>();
-        Set<String> wordSet = new HashSet<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("./Input_02.txt"))) {
-            String word;
-            while ((word = br.readLine()) != null) {
-                words.add(word);
-                wordSet.add(word);
-            }
-        }
 
-        words.sort((a, b) -> b.length() - a.length());
-
-        String longest = "", secondLongest = "";
-        long startTime=System.nanoTime();
-
-        for (String word : words) {
-            if (isCompoundWord(word, wordSet)) {
-                if (longest.isEmpty()) {
-                    longest = word;
-                } else {
-                    secondLongest = word;
-                    break;
-                }
-            }
-        }
-        long endTime=System.nanoTime();
-        long displayTime=(endTime-startTime);
-        
-
-        System.out.println("Longest compound word: " + longest);
-        System.out.println("Second longest compound word: " + secondLongest);
-        System.out.println("Time taken: "+displayTime+" ns");
-
-    }
-
-    
-    public static boolean isCompoundWord(String word, Set<String> wordSet) {
+    public static boolean func(String word, Set<String> ws) {
         for (int i = 1; i < word.length(); i++) {
-            if (wordSet.contains(word.substring(0, i)) && 
-               (wordSet.contains(word.substring(i)) || isCompoundWord(word.substring(i), wordSet))) {
+            if (ws.contains(word.substring(0, i)) && 
+               (ws.contains(word.substring(i)) || func(word.substring(i), ws))) {
                 return true;
             }
         }
         return false;
     }
+
+    public static void main(String[] args) throws IOException {
+        List<String> w = new ArrayList<>();
+        Set<String> ws = new HashSet<>();
+        try (BufferedReader buff = new BufferedReader(new FileReader("./Input_02.txt"))) {
+            String word;
+            while ((word = buff.readLine()) != null) {
+                w.add(word);
+                ws.add(word);
+            }
+        }
+
+        w.sort((a, b) -> b.length() - a.length());
+
+        String longestComp = "", secondlongestComp = "";
+        long startT=System.nanoTime();
+
+        for (String word : w) {
+            if (func(word, ws)) {
+                if (longestComp.isEmpty()) {
+                    longestComp = word;
+                } else {
+                    secondlongestComp = word;
+                    break;
+                }
+            }
+        }
+        long endT=System.nanoTime();
+        double displayT=(endT-startT)*Math.pow(10,-6);
+
+        System.out.println("Longest compound word: " + longestComp);
+        System.out.println("Second Longest compound word: " + secondlongestComp);
+        System.out.println("Time taken: "+displayT+" ms");
+
+    }
+
+    
+    
 }
